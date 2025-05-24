5438188488ee4e2ab3f588ce48e0ad85e07a4254
LoganForUp <790040400@qq.com>
2025-05-24 13:04:30 +0800

    AddAuraContextAnd InputModeTo AuraPlayerController

--- Source/Aura/Private/PlayerController/AuraPlayerController.cpp
+++ Source/Aura/Private/PlayerController/AuraPlayerController.cpp
@@ -2,3 +2,27 @@
 
 
 #include "PlayerController/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+		GetLocalPlayer());
+	check(SubSystem);
+	SubSystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	FInputModeGameAndUI InputMode;
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputMode.SetHideCursorDuringCapture(false);
+	SetInputMode(InputMode);
+}
