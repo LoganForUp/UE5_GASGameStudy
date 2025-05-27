7f67a0c410cf9ed86a8e2319616c1c19121fe50c
LoganForUp <790040400@qq.com>
2025-05-27 22:39:55 +0800

    SetReplicationMode

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -8,12 +8,12 @@
 
 AAuraPlayerState::AAuraPlayerState()
 {
-	SetNetUpdateFrequency(100.f);
-
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
-
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
+	SetNetUpdateFrequency(100.f);
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
